2c877a3b73b2ee25ac9a124393befe3788581d28
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-04-11 09:06:29 -0700

    scorekeeper

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,13 +5,15 @@ const option1El = document.getElementById("A");
 const option2El = document.getElementById("B");
 const option3El = document.getElementById("C");
 const option4El = document.getElementById("D");
-const questionForm = document.getElementById("question/answer")
-const instructions = document.querySelector(".card-header")
+const questionForm = document.getElementById("question/answer");
+const instructions = document.querySelector(".card-header");
 const queContainerEl = document.querySelector(".question-container");
+const feedbackEl = document.querySelector("#feedback");
 
 var timer;
-var timeLeft = 20;
+var timeLeft = 100;
 var questionIndex;
+var score = 0;
 
 const q1 = {
     question: "Which of the following is not a primitive data type in JavaScript?",
@@ -19,7 +21,7 @@ const q1 = {
     optionB: "(B) Number",
     optionC: "(C) Boolean",
     optionD: "(D) Object",
-    Answer: "D",
+    Answer: "D"
 }
 const q2 = {
     question: "Which of the following is the correct syntax to declare a variable in JavaScript?",
@@ -35,7 +37,7 @@ const q3 = {
     optionB: "(B) functionName();",
     optionC: "(C) both (A) and (B)",
     optionD: "(D) none of the above",
-    Answer: "optionC"
+    Answer: "B"
 }
 const q4 = {
     question: "Which of the following is the correct syntax to create an array in JavaScript?",
@@ -43,7 +45,7 @@ const q4 = {
     optionB: "(B) let arrayName = [];",
     optionC: "(C) const arrayName = [];",
     optionD: "(D) all of the above",
-    Answer: "optionD"
+    Answer: "D"
 }
 const q5 = {
     question: "Which of the following is the correct syntax to add an element to an array in JavaScript?",
@@ -51,7 +53,7 @@ const q5 = {
     optionB: "(B) arrayName.unshift(element);",
     optionC: "(C) both (A) and (B)",
     optionD: "(D) none of the above",
-    Answer: "optionA"
+    Answer: "A"
 }
 const q6 = {
     question: "Which of the following is the correct syntax to remove and return the last element from an array in JavaScript?",
@@ -59,15 +61,15 @@ const q6 = {
     optionB: "(B) arrayName.shift();",
     optionC: "(C) both (A) and (B)",
     optionD: "(D) none of the above",
-    Answer: "optionA"
+    Answer: "A"
 }
 const q7 = {
     question: "Which of the following is the correct syntax to iterate through an array in JavaScript?",
     optionA: "(A) for (var i = 0; i < arrayName.length; i++) {<code>}",
     optionB: "(B) for (let i = 0; i < arrayName.length; i--) {<code>}",
-    optionC: "(C) for (const i = 0; i < arrayName; i++) {<code>}",
+    optionC: "(C) for (const i = 0; i > arrayName; i++) {<code>}",
     optionD: "(D) all of the above",
-    Answer: "optionA"
+    Answer: "A"
 }
 const q8 = {
     question: "Which of the following is the correct syntax to check if an element is present in an array in JavaScript?",
@@ -75,7 +77,7 @@ const q8 = {
     optionB: "(B) arrayName.includes(element);",
     optionC: "(C) both (A) and (B)",
     optionD: "(D) none of the above",
-    Answer: "optionC"
+    Answer: "C"
 }
 const q9 = {
     question: "Which of the following is the correct syntax to convert a string to a number in JavaScript?",
@@ -83,7 +85,7 @@ const q9 = {
     optionB: "(B) parseInt(string);",
     optionC: "(C) both (A) and (B)",
     optionD: "(D) none of the above",
-    Answer: "optionB"
+    Answer: "B"
 }
 const q10 = {
     question: "Which of the following is the correct syntax to convert a number to a string in JavaScript?",
@@ -91,7 +93,7 @@ const q10 = {
     optionB: "(B) String(number);",
     optionC: "(C) both (A) and (B)",
     optionD: "(D) none of the above",
-    Answer: "optionD"
+    Answer: "D"
 }
 
 var questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
@@ -102,20 +104,26 @@ var questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
 
 
 function endgame() {
-
+    console.log("endgame")
+    timeEl.textContent = "---";
 }
+
 function renderOptions() {
 
 }
+
 function correctChoice() {
 
 }
+
 function wrongChoice() {
 
 }
+
 function scoreTracker() {
 
 }
+
 function checkAnswer(event) {
     var userOption = event.target;
     console.log(userOption);
@@ -124,42 +132,40 @@ function checkAnswer(event) {
         console.log(attrSelection)
   //logic for right or wrong ans 
         if(attrSelection == questions[questionIndex].Answer){
-            console.log("correct ans ");
+            console.log("correct ans");
+            feedbackEl.textContent = "Correct!";
+            score++
+            console.log("score", score);
         }else{
-            console.log("wrong selection");
+            console.log("wrong ans");
+            feedbackEl.textContent = "Wrong";
+            timeLeft-=10;
         }
     }
-
     questionIndex++;
     renderQuestion();
-    
 }
+
 function renderQuestion() {
-    // questionEl.textContent = q1.question;
-    // option1El.textContent = q1.optionA;
-    // option2El.textContent = q1.optionB;
-    // option3El.textContent = q1.optionC;
-    // option4El.textContent = q1.optionD;
-    //for ( let i = 0; i < questions.length; i++) {
-        console.log("Que Index",  questionIndex )
-        questionEl.textContent = questions[questionIndex].question;
-        option1El.textContent = questions[questionIndex].optionA;
-        option2El.textContent = questions[questionIndex].optionB;
-        option3El.textContent = questions[questionIndex].optionC;
-        option4El.textContent = questions[questionIndex].optionD;
-       // checkAnswer();
-    //};
-    
-    //checkAnswer();
-}
+    console.log("Que Index",  questionIndex )
+    questionEl.textContent = questions[questionIndex].question;
+    option1El.textContent = questions[questionIndex].optionA;
+    option2El.textContent = questions[questionIndex].optionB;
+    option3El.textContent = questions[questionIndex].optionC;
+    option4El.textContent = questions[questionIndex].optionD;
+    // if (questionIndex=10) {
+    //     endgame()
+    // };
+};
+
 function timeOut() {
     timeEl.textContent = "---";
     clearInterval(timer);
-    confirm("You are out of time");
+    confirm("You are out of time. Click ok to see your score.");
     // Actions once time is out
-    /*if (confirm == true) {
-          endgame();
-      }*/
+    if (confirm == true) {
+        endgame();
+    };
 };
 // called when page loads for highscores from local storage
 function init() {
@@ -177,11 +183,12 @@ function startTimer() {
         };
     }, 1000);
 };
-// starts/ends timer (calls first question) 
+
+// starts timer (calls first question) 
 function startquiz() {
     console.log("start-quiz");
-    //startEl.disabled = true;
     startTimer();
+    queContainerEl.style.display = "flex";
     instructions.style.display = "none";
     startEl.style.display = "none";
     questionIndex=0;
